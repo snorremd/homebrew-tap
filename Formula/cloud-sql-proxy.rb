4dbef53107ddf360a50228fb441edfe0a3dfd6e1
Snorre Magnus Davøen <post@snorre.io>
2023-04-28 12:46:12 +0200

    Initial gcp cloud sql proxy formula and tap
    Adds new tap using the "brew tap-new" command. An initial cloud proxy
    sql formulae has been added using "brew create" with the go template.
    Note that there are currently no tests for the formulae.
    
    The goal here is simply to make a simple brew tap to install common
    tools that I want to keep updated.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

---
+++ Formula/cloud-sql-proxy.rb
@@ -0,0 +1,17 @@
+class CloudSqlProxy < Formula
+  desc "Utility for secure connections to your GCP SQL instances"
+  homepage "https://github.com/GoogleCloudPlatform/cloud-sql-proxy"
+  url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "83c3e801d9c153af47c74686e9d900e04adcbdb685328aad194bf91b55adc17c"
+  license ""
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Snorremd Tap
+
+## How do I install these formulae?
+
+`brew install snorremd/tap/<formula>`
+
+Or `brew tap snorremd/tap` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
