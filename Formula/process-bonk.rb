603d11bdaf450f9d0e960e6737ea894d88a1fe91
Snorre Magnus Davøen <snorre.magnus.davoen@adventuretech.no>
2024-01-15 22:52:11 +0100

    Clean up formula file and just return true for test

--- Formula/process-bonk.rb
+++ Formula/process-bonk.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ProcessBonk < Formula
   desc "BONK process, don't eat all my Mac's CPU"
   homepage ""
@@ -11,19 +8,16 @@ class ProcessBonk < Formula
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", *std_cargo_args
   end
 
   service do
-    # Run with config file in /opt/homebrew/etc/process-bonk.toml
     run [opt_bin/"process-bonk",  "#{etc}/process-bonk/process-bonk.toml"]
     log_path var/"log/process-bonk.log"
     error_log_path var/"log/process-bonk.log"
   end
 
   def post_install
-    # Install the config file
     pkgetc.install "process-bonk.toml"
   end
 
@@ -41,15 +35,6 @@ class ProcessBonk < Formula
 
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test process-bonk`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end
